1c53b9b202ec76fdd0c2d259d68de920c8aeedb0
Randi <rlmendel@uw.edu>
2015-10-19 01:15:33 -0700

    finalize

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 // Function to draw your map
 var drawMap = function() {
-	var map = L.map('map').setView([38, 97], 3);
+	var map = L.map('map').setView([39, -98], 4);
 
   // Create a tile layer variable using the appropriate url
 	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {minZoom:0});
@@ -9,46 +9,117 @@ var drawMap = function() {
 	layer.addTo(map); 
 
   // Execute your function to get data
-	getData();
+	getData(map);
 }
 
 // Function for getting data
-var getData = function() {
-
+var getData = function(map) {
   // Execute an AJAX request to get the data in data/response.js
-  //var data = $.get('data/response.json';
-  //var circle = L.circle([38,97], 20);
-  // When your request is successful, call your customBuild function
-  //.done(custombuild());
+
+  	$.ajax({
+  		dataType:'json',
+  		success: function(data) {
+
+  			customBuild(data, map);
+  		},
+  		url: 'data/response.json'
+  	});
+
 }
 
+  // When your request is successful, call your customBuild function
+
+
 // Loop through your data and add the appropriate layers and points
-var customBuild = function() {
+var customBuild = function(data, map) {
+
+	var maleskilled = 0;
+	var malesalive = 0;
+	var otherskilled = 0;
+	var othersalive = 0;
+	
+	var circlesize = 10; //circle radius is 6 for killed, changes to 3 for not killed
 	// Be sure to add each layer to the map
 	//male
-	window.alert("hello");
-	var malevictim = new L.LayerGroup(['lat', 'lng'], 10, {color:'blue'});
+	var malevictim = new L.LayerGroup([]);
 	//female
-	var femalevictim = new L.layerGroup(['lat', 'lng'], 10, {color:'pink'});
+	var femalevictim = new L.layerGroup([]);
 	//unknown
-	var unspecified - new L.layerGroup(['lat', 'lng'], 10, {color:'gray'});
+	var unspecified = new L.layerGroup([]);
 
-	$.each(data, {L.cirlce(['lat', 'lng'], 20, { 
-		if ("victim's gender".equals('male') {
-			this.addTo(malevictim);
-		} else if ("victim's gender".equals('female')) {
-			this.addTp(femalevictim);
+
+	var circlecolor = null;
+	var addwhere = null;
+
+	$.each(data, function() { 
+		var lat = $(this).attr("lat");
+		var lng = $(this).attr("lng");
+
+		if ($(this).attr("Victim's Gender") == 'Male') {
+			//if killed add to males killed 
+			if ($(this).attr("Hit or Killed?") == 'Killed') {
+				circlesize = 10;
+				maleskilled++;
+			} else { //if not killed add to males alive
+				malesalive++;
+				circlesize = 5;
+			}
+			circlecolor = 'blue';
+			addwhere = malevictim;
+
+		} else if ($(this).attr("Victim's Gender") == 'Female') {
+			if($(this).attr("Hit or Killed?") == 'Killed'){
+				circlesize = 10;
+				otherskilled++;
+			} else {
+				othersalive++;
+				circlesize = 5;
+			}
+			circlecolor = 'pink';
+			addwhere = femalevictim;
+
 		} else {
-			this.addTo(unspecified);
+			//if killed, add to total women/other killed
+			if($(this).attr("Hit or Killed?") == 'Killed') {
+				otherskilled++;
+			} else { //if not killed, add to total women/other alive
+				othersalive++;
+			}
+			circlecolor = 'grey';
+			addwhere = unspecified;
+
 		} 
-	);.get
 
-	malevictim.addTo(map);
-	femalevictim.addTo(map);
-	unspecified.addTo(map);
+		//adds circle marker for each incident, at location given
+		var circle = new L.circleMarker([lat,lng], {radius:circlesize, color:circlecolor});
+		circle.addTo(addwhere);
 
+		//link of information source for given incident
+		var str = "View Source";
+		var sourceLink = str.link($(this).attr('Source Link'));
 	
+		//adds popup for more information on incident story, and link
+		circle.bindPopup($(this).attr("Summary") + sourceLink);
+
+
+	});
+
+
+
+	var genderOverlay = {
+    	"Male" : malevictim,
+    	"Female" : femalevictim,
+    	"Unspecified" : unspecified
+	};
+
 	// Once layers are on the map, add a leaflet controller that shows/hides layers
+	L.control.layers(null,genderOverlay).addTo(map);
+
+	$("#malesalive").text(malesalive);
+	$("#maleskilled").text(maleskilled);
+	$("#othersalive").text(othersalive);
+	$("#otherskilled").text(otherskilled);
+
   
 }
 
